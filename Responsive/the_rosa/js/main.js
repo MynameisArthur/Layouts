69ffa8f6148686321404d1b5ab7f4f2325780e1d
Arthur <arthurd800@wp.pl>
2020-04-10 14:03:31 +0200

    finished project

--- Responsive/the_rosa/js/main.js
+++ Responsive/the_rosa/js/main.js
@@ -4,3 +4,12 @@ const menuToggler = document.querySelector('.menu-toggle');
 menuToggler.addEventListener('click',()=>{
     document.body.classList.toggle('open');
 });
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',{origin: 'left',duration: 1000,distance: '25rem', delay: 300});
+sr.reveal('.animate-right',{origin: 'right',duration: 1000,distance: '25rem', delay: 600});
+sr.reveal('.animate-top',{origin: 'top',duration: 1000,distance: '25rem', delay: 600});
+sr.reveal('.animate-bottom',{origin: 'bottom',duration: 1000,distance: '25rem', delay: 600});
+
+
