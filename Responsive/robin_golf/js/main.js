6b09533c1a7288278717a718ed6c2d96424e9358
Arthur <arthurd800@wp.pl>
2020-05-04 13:12:36 +0200

    styled responsivness in section 1 and 2, starterd styling section 3 with the slider, added sass files

--- Responsive/robin_golf/js/main.js
+++ Responsive/robin_golf/js/main.js
@@ -1,6 +1,9 @@
 const navContainer = document.querySelector('.nav-container');
 const menuToggler = document.querySelector('.mobile-menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
+const slider = document.querySelector('.slider');
+const roundBtns = slider.querySelectorAll('.round-btn');
+
 
 window.addEventListener('scroll',(e)=>{
     if(window.scrollY > 150)
@@ -19,7 +22,7 @@ function openMobileMenu()
     if(mobileNav.classList.contains('open')){
         mobileNav.style.animation = `slide-up 0.9s forwards`;      
         menuToggler.removeEventListener('click',openMobileMenu);    
-    }   
+    } 
     mobileNav.classList.toggle('open');
     setTimeout(()=>{
             mobileNav.style.animation = ``;
@@ -27,5 +30,18 @@ function openMobileMenu()
         },900);  
 }
 
+roundBtns.forEach(btn=>btn.addEventListener('click',(e)=>{
+    const current = slider.querySelector('.current-slide');
+    const slides = slider.querySelectorAll('.slider-container_item');
+    let currentIndex = [...slides].indexOf(current);
+    if(currentIndex === 2){
+        currentIndex = -1;
+    } 
+    // else if(currentIndex === 0)
+    // {
+    //     currentIndex = 2;
+    // }
+    current.classList.remove('current-slide');
+    slides[currentIndex + 1].classList.add('current-slide');
     
-
+}));
